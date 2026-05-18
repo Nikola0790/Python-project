a12327f7dc725ccebfa86abb799d8fc5c55bbf9c
Nikola Drcalic <nikola@Nikolas-MacBook-Pro.local>
2026-05-18 23:16:35 +0200

    Exercises - Models

--- django/django_1/admin.py
+++ django/django_1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Article
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'content', 'status', 'date_added', 'publish_date', 'removal_date')
--- django/django_1/models.py
+++ django/django_1/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+GENRE_OPTIONS = ((-1, 'not defined'), (0, "rock"), (1, 'metal'), (2, 'pop'), (3, 'hip-hop'), (4, 'electronic'), (5, 'reggae'), (6, 'other'))
+class Band(models.Model):
+    name = models.CharField(max_length=255)
+    year = models.IntegerField()
+    still_active = models.BooleanField(default=True)
+    genre = models.IntegerField(choices=GENRE_OPTIONS, default=-1)
+
+    def __str__(self):
+        return self.name
+    
+class Album(models.Model):
+    SCALE = ((0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
+
+    album_title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    rating = models.IntegerField(choices=SCALE, default=0)
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(null=True)
+
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('in writing', 'in writing'),
+        ('pending editor approval', 'pending editor approval'),
+        ('published', 'published'),
+    )
+    
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=64, null=True)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=50, 
+        choices=STATUS_CHOICES, 
+        default='in writing'
+    )
+    publish_date = models.DateField(null=True, blank=True)
+    removal_date = models.DateField(null=True, blank=True)
