357b26d637a01c5815b050a7a52cff8ad4dfae73
Nikola Drcalic <nikola@Nikolas-MacBook-Pro.local>
2026-06-01 00:03:20 +0200

    Add booking model

--- room-booking-app/booking/models.py
+++ room-booking-app/booking/models.py
@@ -4,3 +4,10 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     capacity = models.IntegerField()
     projector_availability = models.BooleanField(default=False)
+class Booking(models.Model):
+    date = models.DateField()
+    comment = models.TextField(blank=True, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='bookings')
+
+    class Meta:
+        unique_together = ('date', 'room')
